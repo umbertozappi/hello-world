31a0d644488a27ab334c819ce6686eb3d9b00475
Umberto Zappi <50056694+umbertozappi@users.noreply.github.com>
2019-04-28 00:09:06 +0200

    Add new .c file
    Main file for printing "Hello World!"

---
+++ hello-world.c
@@ -0,0 +1,12 @@
+/*************************************
+ * Examlpe file .c of Hello World!
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Hello World!\n");
+	return 0;
+}
